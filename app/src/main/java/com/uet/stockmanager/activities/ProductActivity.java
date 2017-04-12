953ca8508536eb4d17e0781a64496ba7c4459567
SyPhamWSC <sy.phamtiennd@gmail.com>
2017-04-12 17:57:37 +0700

    update add more quanlity when click to item product

--- app/src/main/java/com/uet/stockmanager/activities/ProductActivity.java
+++ app/src/main/java/com/uet/stockmanager/activities/ProductActivity.java
@@ -1,6 +1,5 @@
 package com.uet.stockmanager.activities;
 
-import android.app.Dialog;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
@@ -8,16 +7,16 @@ import android.content.IntentFilter;
 import android.content.ServiceConnection;
 import android.graphics.Color;
 import android.graphics.drawable.ColorDrawable;
-import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.Gravity;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
-import android.widget.Button;
-import android.widget.EditText;
+import android.widget.AdapterView;
 import android.widget.ListView;
 import android.widget.Toast;
 
@@ -25,6 +24,7 @@ import com.uet.stockmanager.R;
 import com.uet.stockmanager.adapters.ProductAdapter;
 import com.uet.stockmanager.application.AppController;
 import com.uet.stockmanager.dialogs.AddProductDialog;
+import com.uet.stockmanager.dialogs.EditProductDialog;
 import com.uet.stockmanager.models.Product;
 import com.uet.stockmanager.models.ProductDao;
 
@@ -33,14 +33,17 @@ import java.util.List;
 import butterknife.BindView;
 import butterknife.ButterKnife;
 
-public class ProductActivity extends AppCompatActivity{
+public class ProductActivity extends AppCompatActivity {
 
     private static final String DIALOG_TITLE = "Add New Product";
-    private static final String UPDATE_DATABASE = "update";
+    private static final String ADD_NEW_PRODUCT = "update";
     private static final String ADD_NAME_PRODUCT = "name";
     private static final String ADD_CATEGORY_PRODUCT = "category";
     private static final String ADD_PRICE_PRODUCT = "price";
     private static final String ADD_QUANLITY_PRODUCT = "quanlity";
+    private static final String UPDATE_PRODUCT = "update product";
+    private static final String TAG = "ProductActivity";
+    private static final String ADD_MORE = "add more";
 
     @BindView(R.id.lv_main)
     ListView lvMain;
@@ -50,7 +53,8 @@ public class ProductActivity extends AppCompatActivity{
     private ProductDao pDao;
     private List<Product> productList;
     private ProductAdapter productAdapter;
-    private BroadcastReceiver updateDatabase;
+    private BroadcastReceiver addNewProduct = new MyBroadcast();
+    private Product productTemp;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -66,29 +70,29 @@ public class ProductActivity extends AppCompatActivity{
 
         pDao = ((AppController) getApplication()).getDaoSession().getProductDao();
         productList = pDao.queryBuilder().list();
-        productAdapter = new ProductAdapter(this,productList);
+        productAdapter = new ProductAdapter(this, productList);
         lvMain.setAdapter(productAdapter);
-
-        updateDatabase = new BroadcastReceiver() {
+        lvMain.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
-            public void onReceive(Context context, Intent intent) {
-                String name = intent.getStringExtra(ADD_NAME_PRODUCT);
-                String category = intent.getStringExtra(ADD_CATEGORY_PRODUCT);
-                int price = intent.getIntExtra(ADD_PRICE_PRODUCT,1);
-                int quanlity = intent.getIntExtra(ADD_QUANLITY_PRODUCT,1);
-
-                Product product = new Product();
-                product.setName(name);
-                product.setCategory(category);
-                product.setPrice(price);
-                product.setQuantity(quanlity);
-
-                updateListProduct(product);
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                EditProductDialog editProductDialog = new EditProductDialog(ProductActivity.this, R.style.PauseDialogAnimation);
+                editProductDialog.getWindow().getAttributes().windowAnimations = R.style.PauseDialogAnimation;
+                editProductDialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+                long idQuery = position + 1;
+                Product product = pDao.queryBuilder().where(ProductDao.Properties.Id.eq(idQuery)).unique();
+                Log.i(TAG, "Product name: " + product.getName());
+                Toast.makeText(ProductActivity.this,"Add more " + product.getName(),Toast.LENGTH_LONG).show();
+                productTemp = product;
+                editProductDialog.setTitle("");
+                editProductDialog.show();
             }
-        };
+        });
 
-        IntentFilter filter = new IntentFilter(UPDATE_DATABASE);
-        this.registerReceiver(updateDatabase,filter);
+        //add new product
+        IntentFilter filter = new IntentFilter();
+        filter.addAction(ADD_NEW_PRODUCT);
+        filter.addAction(UPDATE_PRODUCT);
+        this.registerReceiver(addNewProduct, filter);
     }
 
     private void initViews() {
@@ -104,9 +108,9 @@ public class ProductActivity extends AppCompatActivity{
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        switch (item.getItemId()){
+        switch (item.getItemId()) {
             case R.id.menu_add_product:
-                AddProductDialog addProductDialog = new AddProductDialog(this,R.style.PauseDialogAnimation);
+                AddProductDialog addProductDialog = new AddProductDialog(this, R.style.PauseDialogAnimation);
                 addProductDialog.getWindow().getAttributes().windowAnimations = R.style.PauseDialogAnimation;
                 addProductDialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
                 addProductDialog.setTitle(DIALOG_TITLE);
@@ -124,12 +128,42 @@ public class ProductActivity extends AppCompatActivity{
     @Override
     protected void onDestroy() {
         super.onDestroy();
-        this.unbindService((ServiceConnection) this.updateDatabase);
+        this.unbindService((ServiceConnection) this.addNewProduct);
     }
 
-    private void updateListProduct(Product product){
+    private void updateListProduct(Product product) {
         pDao.insert(product);
         productList.add(product);
         productAdapter.notifyDataSetChanged();
+    }
+
+    private class MyBroadcast extends BroadcastReceiver {
+
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            if (intent.getAction().equals(ADD_NEW_PRODUCT)) {
+                String name = intent.getStringExtra(ADD_NAME_PRODUCT);
+                String category = intent.getStringExtra(ADD_CATEGORY_PRODUCT);
+                int price = intent.getIntExtra(ADD_PRICE_PRODUCT, 1);
+                int quanlity = intent.getIntExtra(ADD_QUANLITY_PRODUCT, 1);
+
+                Product product = new Product();
+                product.setName(name);
+                product.setCategory(category);
+                product.setPrice(price);
+                product.setQuantity(quanlity);
+
+                updateListProduct(product);
+            }
+            if (intent.getAction().equals(UPDATE_PRODUCT)) {
+                int quanlity = intent.getIntExtra(ADD_MORE,1);
+                productTemp.setQuantity(productTemp.getQuantity() + quanlity);
+                pDao.update(productTemp);
+                int id = (int) (productTemp.getId() - 1);
+                productList.get(id).setQuantity(productTemp.getQuantity() + quanlity);
+                productAdapter.notifyDataSetChanged();
+            }
+
+        }
     }
 }
---
+++ app/src/main/java/com/uet/stockmanager/dialogs/EditProductDialog.java
@@ -0,0 +1,52 @@
+package com.uet.stockmanager.dialogs;
+
+
+import android.app.Dialog;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.StyleRes;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.uet.stockmanager.R;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+public class EditProductDialog extends Dialog{
+
+    private static final String UPDATE_PRODUCT = "update product";
+    private static final String ADD_MORE = "add more";
+
+    @BindView(R.id.edt_edit_product_quanlity) EditText edtEditText;
+    @BindView(R.id.btn_edit_product) Button btnUpdateEdited;
+
+    public EditProductDialog(@NonNull Context context, @StyleRes int themeResId) {
+        super(context, themeResId);
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.dialog_edit_product);
+
+        ButterKnife.bind(this);
+
+        btnUpdateEdited.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(edtEditText.getText().toString().equals("")){
+                    Toast.makeText(getContext(),"Quanlity cann't be empty!!!", Toast.LENGTH_SHORT);
+                }
+                Intent mIntent = new Intent(UPDATE_PRODUCT);
+                mIntent.putExtra(ADD_MORE,String.valueOf(edtEditText.getText().toString()));
+                getContext().sendBroadcast(mIntent);
+                dismiss();
+            }
+        });
+    }
+}
